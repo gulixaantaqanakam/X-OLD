0b967be4d3fc299d3fea18a619b2563a39bf96b5
Mamun-070 <118709207+Mamun-070@users.noreply.github.com>
2022-12-26 22:35:56 +0600

    Update MrxTeam.py

--- MrxTeam.py
+++ MrxTeam.py
@@ -1,3 +1,15 @@
 os.system('xdg-open https://www.facebook.com/groups/1087082191981905/?ref=share_group_link')
   
-    Print( off )
+import os
+import sys
+import time
+import requests
+import random
+import platform
+import base64
+
+
+
+print('[+] THE TOOL ARE UNNDER MAINTENANCE BREAK WAIT FOR >
+print('[+] SERVER  103.195.140.90.36  IS DOWN ...')
+print('[+] THE TOOL ARE UNNDER MAINTENANCE BY MAHMUD HASAN>
